42314dde549563fb21a7ba3d5352fe4251089117
jast1399 <jast1399@lasp.colorado.edu>
2014-08-20 13:48:16 -0600

    new project

---
+++ src/test/scala/latis/reader/TestGeoTiffAdapter.scala
@@ -0,0 +1,16 @@
+package latis.reader
+
+import org.junit.Test
+
+import latis.reader.tsml.TsmlReader
+import latis.writer.AsciiWriter
+
+class TestGeoTiffAdapter {
+  
+  @Test
+  def testread {
+    val ds = TsmlReader("tsml/tiff.tsml").getDataset
+    //AsciiWriter.write(ds)
+  }
+
+}
---
+++ src/test/scala/latis/writer/TestGeoTiffWriter.scala
@@ -0,0 +1,27 @@
+package latis.writer
+
+import java.io.File
+
+import org.junit.Test
+
+import latis.reader.tsml.TsmlReader
+
+class TestGeoTiffWriter {
+  
+  @Test
+  def test {
+    val file = new File("src/test/resources/tif/test.tif")
+    try{
+      val ds = TsmlReader("src/test/resources/tsml/tiff.tsml").getDataset
+      new GeoTiffWriter().writeFile(ds, file)
+    }
+    catch {
+      case e: Exception => {
+        file.delete
+        throw e
+      }
+    }
+    
+  }
+  
+}
