b9c90c85cd261f14799c8d4762d9a6a9c43137ee
jast1399 <jast1399@lasp.colorado.edu>
2016-01-19 19:22:32 +0000

    finish up the ECEF -> WGS84 transform

--- src/main/scala/latis/ops/Transform.scala
+++ src/main/scala/latis/ops/Transform.scala
@@ -1,54 +1,58 @@
 package latis.ops
 
-import latis.dm._
-import com.vividsolutions.jts.geom.Coordinate
-import org.opengis.referencing.crs.CoordinateReferenceSystem
 import org.geotools.geometry.jts.JTS
 import org.geotools.referencing.CRS
-import latis.util.iterator.MappingIterator
-import scala.collection.JavaConversions._
-import org.geotools.referencing.crs.DefaultGeographicCRS
+import org.geotools.referencing.crs.DefaultGeocentricCRS
+import org.opengis.referencing.crs.CoordinateReferenceSystem
 
-class Transform(var sourceCRS: CoordinateReferenceSystem, 
-    targetCRS: CoordinateReferenceSystem) extends Operation {
+import com.vividsolutions.jts.geom.Coordinate
+
+import latis.dm.Function
+import latis.dm.Number
+import latis.dm.Real
+import latis.dm.Sample
+import latis.dm.Tuple
+import latis.dm.Variable
+import latis.metadata.Metadata
+import latis.util.iterator.MappingIterator
+
+/**
+ * Transform from ECEF to WGS84 3D.
+ */
+class Transform extends Operation {
     
+  val sourceCRS: CoordinateReferenceSystem = DefaultGeocentricCRS.CARTESIAN //EPSG:4978
+  val targetCRS: CoordinateReferenceSystem = CRS.decode("epsg:4979") //WGS84 3D
+  
   lazy val transform = CRS.findMathTransform(sourceCRS, targetCRS)
   
   override def applyToSample(sample: Sample): Option[Sample] = {
     val coord = sample.domain match {
-      case Tuple(Seq(Number(x), Number(y))) => new Coordinate(x,y)
       case Tuple(Seq(Number(x), Number(y), Number(z))) => new Coordinate(x,y,z)
       case _ => throw new UnsupportedOperationException(
-          "A function must have a domain of the form Tuple(x,y) or Tuple(x,y,z) to be transformed")
+          "A function must have a domain of the form Tuple(x,y,z) to be transformed")
     }
     
     JTS.transform(coord, coord, transform)
     
-    val dom = if(coord.z.isNaN) Tuple(Real(coord.x), Real(coord.y))
-      else Tuple(Real(coord.x), Real(coord.y), Real(coord.z))
+    val dom = Tuple(Real(Metadata("lat"), coord.x), 
+                    Real(Metadata("lon"), coord.y), 
+                    Real(Metadata("alt"), coord.z))
     
     Some(Sample(dom, sample.range))
   }
   
   /**
-   * Override to get the source CRS and set the new CRS Metadata.
+   * Override to set the new CRS Metadata.
    */
   override def applyToFunction(function: Function): Option[Variable] = {
-    function.getMetadata("epsg") match {
-      case Some(s) => sourceCRS = CRS.decode(s"epsg:$s")
-      case None =>
-    }
-    
     val mit = new MappingIterator(function.iterator, (s: Sample) => this.applyToSample(s))
     val template = mit.peek match {
       case null => function.getSample //empty iterator so no-op
       case s => s
     }
     
-    val md = targetCRS.getIdentifiers.find(_.getCodeSpace == "EPSG") match {
-      case Some(id) => function.getMetadata + ("epsg", id.getCode)
-      case None => function.getMetadata
-    }
+    val md = function.getMetadata + ("epsg" -> "4979")
     
     Some(Function(template.domain, template.range, mit, md))
   }
@@ -57,14 +61,6 @@ class Transform(var sourceCRS: CoordinateReferenceSystem,
 
 object Transform extends OperationFactory {
   
-  def apply(scode: String, tcode: String) = new Transform(CRS.decode(s"epsg:$scode"), CRS.decode(s"epsg:$tcode"))
-  
-  def apply(tcode: String) = new Transform(DefaultGeographicCRS.WGS84_3D, CRS.decode(s"epsg:$tcode"))
-  
-  override def apply(args: Seq[String]) = args.length match {
-    case 1 => Transform(args.head)
-    case 2 => Transform(args.head, args.last)
-    case _ => ???
-  }
+  override def apply() = new Transform
   
 }
---
+++ src/test/scala/latis/ops/TestTransform.scala
@@ -0,0 +1,28 @@
+package latis.ops
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+import latis.dm.Dataset
+import latis.dm.Function
+import latis.dm.Real
+import latis.dm.Sample
+import latis.dm.Tuple
+import latis.metadata.Metadata
+
+class TestTransform {
+  
+  @Test
+  def test {
+    val t = Transform()
+    val f = Dataset(Function(Seq(Sample(Tuple(Seq(Real(Metadata("x"), 996208.9809235458),
+                                Real(Metadata("y"), -4541094.921570469),
+                                Real(Metadata("z"), 4351898.060411415))), Real(Metadata("foo"),0.0)))))
+    val ds = t(f)
+    val data = ds.toDoubleMap
+    assertEquals(43.301, data("lat")(0), 0.0000001)
+    assertEquals(-77.626667, data("lon")(0), 0.00000000001)
+    assertEquals(0, data("alt")(0), 0.01)
+  }
+  
+}
