d42b814055e109f8dcfc2f27b9cc4cc5c4b183ac
Nic Flores <flores314@yahoo.com>
2016-03-10 23:19:22 -0700

    created empty test file for now

---
+++ src/test/scala/latis/util/TestCirlceMarkPointStyle.scala
@@ -0,0 +1,13 @@
+package latis.util
+
+import java.io.File
+import org.junit.{Test,Ignore}
+//import latis.reader.tsml.TsmlReader
+
+class TestCircleMarkPointStyle {
+  
+  @Ignore @Test
+  def test {
+    val x = 1
+  }
+}
