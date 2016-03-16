cf23d5de9acf84a273c0a31402159812c99a25c3
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-03-16 17:25:43 -0600

    apply the transform at the tuple level

--- src/main/scala/latis/ops/Transform.scala
+++ src/main/scala/latis/ops/Transform.scala
@@ -26,21 +26,37 @@ class Transform extends Operation {
   
   lazy val transform = CRS.findMathTransform(sourceCRS, targetCRS)
   
-  override def applyToSample(sample: Sample): Option[Sample] = {
-    val coord = sample.range match {
-      case Tuple(Seq(Number(x), Number(y), Number(z))) => new Coordinate(x,y,z)
-      case _ => throw new UnsupportedOperationException(
-          "A function must have a range of the form Tuple(x,y,z) to be transformed")
+  override def applyToTuple(tuple: Tuple) = tuple match {
+    case Tuple(Seq(Number(x), Number(y), Number(z))) => {
+      
+      //println(CRS.getAxisOrder(sourceCRS)) //INAPPLICABLE
+      //println(CRS.getAxisOrder(targetCRS)) //NORTH_EAST
+      
+      val coord = new Coordinate(x,y,z)
+      val tcoord = JTS.transform(coord, null, transform)
+      val tup = Tuple(Real(Metadata("latitude"),  tcoord.x), 
+                      Real(Metadata("longitude"), tcoord.y), 
+                      Real(Metadata("altitude"),  tcoord.z))
+      Some(tup)
     }
-    
-    val tcoord = JTS.transform(coord, null, transform)
-    
-    val ran = Tuple(Real(Metadata("latitude"), tcoord.x), 
-                    Real(Metadata("longitude"), tcoord.y), 
-                    Real(Metadata("altitude"), tcoord.z))
-    
-    Some(Sample(sample.domain, ran))
+    case _ => throw new UnsupportedOperationException("The Transform Operation expects a Tuple(x,y,z).")
   }
+  
+//  override def applyToSample(sample: Sample): Option[Sample] = {
+//    val coord = sample.range match {
+//      case Tuple(Seq(Number(x), Number(y), Number(z))) => new Coordinate(x,y,z)
+//      case _ => throw new UnsupportedOperationException(
+//          "A function must have a range of the form Tuple(x,y,z) to be transformed")
+//    }
+//    
+//    val tcoord = JTS.transform(coord, null, transform)
+//    
+//    val ran = Tuple(Real(Metadata("latitude"), tcoord.x), 
+//                    Real(Metadata("longitude"), tcoord.y), 
+//                    Real(Metadata("altitude"), tcoord.z))
+//    
+//    Some(Sample(sample.domain, ran))
+//  }
   
   /**
    * Override to set the new CRS Metadata.
