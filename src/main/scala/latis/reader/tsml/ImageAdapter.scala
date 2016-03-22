8e060667f3b51ca6ddc5c1569587e01bc28433c0
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-03-22 17:18:13 -0600

    Avoid reading too early

--- src/main/scala/latis/reader/tsml/ImageAdapter.scala
+++ src/main/scala/latis/reader/tsml/ImageAdapter.scala
@@ -7,7 +7,8 @@ import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader
 import org.geotools.gce.image.WorldImageReader
 import org.geotools.referencing.CRS
 import org.opengis.referencing.operation.MathTransform2D
-import latis.data.Data
+import latis.data._
+import latis.dm.Function
 import latis.metadata.Metadata
 import latis.reader.tsml.ml.FunctionMl
 import latis.reader.tsml.ml.Tsml
@@ -41,14 +42,14 @@ class ImageAdapter(tsml: Tsml) extends IterativeAdapter[((Double, Double), Array
   }
   
   //get values for a sample domain for the given indices
-  val domain = (xy: (Int, Int)) => {
+  lazy val domain = (xy: (Int, Int)) => {
     val pt = new Point2D.Double(xy._1, xy._2)
     val dom = transform.transform(pt, pt)
     (dom.getX, dom.getY)
   }
   
   //get values for a sample range for the given indices
-  val pixels = (xy: (Int, Int)) => {
+  lazy val pixels = (xy: (Int, Int)) => {
     raster.getPixel(xy._1, xy._2, new Array[Int](raster.getNumBands))
   }
   
@@ -73,21 +74,21 @@ class ImageAdapter(tsml: Tsml) extends IterativeAdapter[((Double, Double), Array
     List(Data(rec._1._1), Data(rec._1._2)) ++ rec._2.map(i => Data(i))
   }
   
-  
   /**
-   * Adds metadata from tsml as well as the width, height, and coordinate reference system of the tiff
+   * Override so we can add width, height, and coordinate reference system metadata.
    */
-  override protected def makeMetadata(vml: VariableMl): Metadata = vml match {
-    case fml: FunctionMl => {
-      val c = CRS.lookupEpsgCode(crs, true) match {
-        case c: Integer => ("epsg" -> c.toString)
-        case null => ("wkt" -> crs.toWKT)
-      }
-      val w = ("width" -> width.toString)
-      val h = ("height" -> height.toString)
-      super.makeMetadata(fml) + c + w + h
+  override def makeFunction(f: Function): Option[Function] = {
+    //Delegate to super to make the Function
+    val f2 = super.makeFunction(f).get
+    
+    //Add metadata
+    val c = CRS.lookupEpsgCode(crs, true) match {
+      case c: Integer => ("epsg" -> c.toString)
+      case null => ("wkt" -> crs.toWKT)
     }
-    case _ => super.makeMetadata(vml)
+    val w = ("width" -> width.toString)
+    val h = ("height" -> height.toString)
+    val md = f.getMetadata + c + w + h
+    Some(Function(f2.getDomain, f2.getRange, md, f2.getData.asInstanceOf[SampledData]))
   }
-  
 }
