5db3e8da53e2994cd16d0df8c9f31d8795d40cf0
Nic Flores <flores314@yahoo.com>
2016-03-23 15:32:15 -0600

    Added test for wind arrow

---
+++ src/test/scala/latis/util/TestWindMarkPointStyle.scala
@@ -0,0 +1,41 @@
+package latis.util
+
+import java.io.File
+import org.junit.{Test,Before,Ignore}
+import org.junit.Assert._
+import org.geotools.styling.StyleFactory
+import org.geotools.factory.CommonFactoryFinder
+import org.opengis.filter.expression.Expression
+import org.opengis.filter.FilterFactory
+import latis.reader.DatasetAccessor
+import latis.dm.Dataset
+import latis.dm.Function
+
+class TestWindMarkPointStyle {
+  var sf: StyleFactory = null
+  var ff: FilterFactory = null
+  var cmps = null
+  var style = null
+  
+  @Before
+  def init {
+    sf = CommonFactoryFinder.getStyleFactory
+    ff = CommonFactoryFinder.getFilterFactory
+  }
+  
+  /*
+   * basic test to make sure style created has set size 10
+   */
+  @Test
+  def circleMarkHasSizeTen {
+    val ds = DatasetAccessor.fromName("gfsanl_wind_files").getDataset()
+    val f = ds match {
+      case Dataset(f: Function) => f
+    }
+    val style = WindMarkPointStyle.getCustomWindSymbolizer(sf,f)
+    val size = ff.literal(10)
+    val rotation = ff.literal(45)
+    assertEquals(size, style.getGraphic.getSize)
+    assertEquals(rotation, style.getGraphic.getRotation)
+  }
+}
