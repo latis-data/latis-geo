8cc9bd9c7a59f761fdf9399182a9b9e1232e0231
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-04-15 12:25:48 -0600

    organize imports

--- src/main/scala/latis/reader/tsml/GeoTiffAdapter.scala
+++ src/main/scala/latis/reader/tsml/GeoTiffAdapter.scala
@@ -1,17 +1,10 @@
 package latis.reader.tsml
 
-import java.awt.geom.Point2D
-import java.awt.image.Raster
-import latis.reader.tsml.ml.Tsml
 import org.geotools.gce.geotiff.GeoTiffReader
-import org.geotools.coverage.grid.GridCoverage2D
-import org.opengis.referencing.operation.MathTransform2D
-import latis.data.Data
-import latis.metadata.Metadata
-import latis.reader.tsml.ml.VariableMl
-import latis.reader.tsml.ml.FunctionMl
-import org.geotools.referencing.CRS
+
 import com.typesafe.scalalogging.LazyLogging
+
+import latis.reader.tsml.ml.Tsml
 
 class GeoTiffAdapter(tsml: Tsml) extends ImageAdapter(tsml) with LazyLogging{
   
