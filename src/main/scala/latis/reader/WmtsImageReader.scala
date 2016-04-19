ccc0ed60926f97475aa837502e37ccf6de9240aa
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-04-19 17:33:36 -0600

    start a reader to return a geo-located image dataset

---
+++ src/main/scala/latis/reader/WmtsImageReader.scala
@@ -0,0 +1,14 @@
+package latis.reader
+
+/**
+ * Use the tile information in the image URL and the WMTSCapabilities.xml
+ * to transform the (row, column) domain to (longitude, latitude).
+ * 
+ * writer properties:
+ *   tile_matrix_dataset
+ *   
+ */
+class WmtsImageReader extends DatasetAccessor {
+  
+  
+}
