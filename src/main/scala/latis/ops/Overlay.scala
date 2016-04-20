6994809f52590af1a97bda91e9a6da4b4e3e8bd1
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-04-20 17:48:09 -0600

    allow Overlay to deal with empty datasets

--- src/main/scala/latis/ops/Overlay.scala
+++ src/main/scala/latis/ops/Overlay.scala
@@ -7,6 +7,8 @@ class Overlay(layer: Dataset) extends Operation {
   
   override def apply(ds: Dataset) = {
     (ds, layer) match {
+      case (ds1: Dataset, ds2: Dataset) if (ds2.isEmpty) => ds1
+      case (ds1: Dataset, ds2: Dataset) if (ds1.isEmpty) => ds2
       case (Dataset(f1: Function), Dataset(f2: Function)) => 
         Dataset(Tuple(f1, f2), ds.getMetadata)
       case (Dataset(Tuple(vs1)), Dataset(f2: Function)) => 
