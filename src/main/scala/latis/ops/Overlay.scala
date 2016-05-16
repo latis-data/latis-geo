8a7c5b5de33487fbc658c8590151616f2973092e
Doug Lindholm <doug.lindholm@lasp.colorado.edu>
2016-05-16 17:19:06 -0600

    manually merge Overlay changes

--- src/main/scala/latis/ops/Overlay.scala
+++ src/main/scala/latis/ops/Overlay.scala
@@ -1,34 +1,38 @@
 package latis.ops
 
-import latis.dm._
-import latis.reader.DatasetAccessor
+import latis.dm.Dataset
+import latis.dm.Function
+import latis.dm.Tuple
+import latis.ops.agg.Join
 
-class Overlay(layer: Dataset) extends Operation {
+class Overlay extends Join {
+  //TODO: generalize (LATIS-489)
+  //  consider a simple join with a Tuple then flatten
+  //TODO: update metadata
   
-  override def apply(ds: Dataset) = {
-    (ds, layer) match {
+  override def apply(base: Dataset, layer: Dataset): Dataset = {
+    (base, layer) match {
       case (ds1: Dataset, ds2: Dataset) if (ds2.isEmpty) => ds1
       case (ds1: Dataset, ds2: Dataset) if (ds1.isEmpty) => ds2
       case (Dataset(f1: Function), Dataset(f2: Function)) => 
-        Dataset(Tuple(f1, f2), ds.getMetadata)
+        Dataset(Tuple(f1, f2), base.getMetadata)
       case (Dataset(Tuple(vs1)), Dataset(f2: Function)) => 
-        Dataset(Tuple(vs1 :+ f2), ds.getMetadata)
+        Dataset(Tuple(vs1 :+ f2), base.getMetadata)
       case (Dataset(f1: Function), Dataset(Tuple(vs2))) => 
-        Dataset(Tuple(f1 +: vs2), ds.getMetadata)
+        Dataset(Tuple(f1 +: vs2), base.getMetadata)
       case (Dataset(Tuple(vs1)), Dataset(Tuple(vs2))) => 
-        Dataset(Tuple(vs1 ++ vs2), ds.getMetadata)
+        Dataset(Tuple(vs1 ++ vs2), base.getMetadata)
     }
   }
   
-  /**
-   * Combine two Overlays into one
-   */
-  def apply(ol: Overlay): Overlay = Overlay(ol(layer))
-  
 }
 
-object Overlay extends OperationFactory {
+object Overlay {
   
-  def apply(ds: Dataset): Overlay = new Overlay(ds)
+  def apply(): Overlay = new Overlay()
+  
+  def apply(ds1: Dataset, ds2: Dataset): Dataset = Overlay()(ds1, ds2)
+  
+  def apply(dss: Seq[Dataset]): Dataset = Overlay()(dss)
   
 }
